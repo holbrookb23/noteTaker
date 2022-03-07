5e1c4422c90dd5fac00c1f016b6a40782d1df2cb
Brian Holbrook <brian.holbrook56@gmail.com>
2022-03-07 15:59:13 -0600

    made it

---
+++ Develop/routes/land.js
@@ -0,0 +1,39 @@
+const fb = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the feedback
+fb.get('/notes', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+
+  
+// POST Route for submitting feedback
+fb.post('/notes', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newFeedback = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newFeedback, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newFeedback,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+module.exports = fb;
